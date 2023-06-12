e2c83676da9b4b2252110fce68ddd75522fc4788
Louis Venter <venter.leoh@gmail.com>
2023-06-12 17:59:14 +0200

    Need to rework Ordered

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from PIL import Image
 import sys
 import floyd_steinberg_dithering
-import ordered_dithering
 
 def main():
-    # floyd_steinberg_dithering.apply(sys.argv[1])
-    ordered_dithering.apply(sys.argv[1])
+    floyd_steinberg_dithering.apply(sys.argv[1])
 
 if __name__ == "__main__":
     main()
